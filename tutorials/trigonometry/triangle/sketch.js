4b74f58c3af955e62cf234c5c614d469d7307879
tegacodes <tega.brain@gmail.com>
2015-05-30 11:57:10 -0400

    new tutorials

--- tutorials/trigonometry/triangle/sketch.js
+++ tutorials/trigonometry/triangle/sketch.js
@@ -1,20 +1,35 @@
 //triangle
-var x1 = 300;
-var y1 = 300;
-var x2, y2;
-var x3, y3;
+var x1 = 20;
+var x2 = x1;
+var y1= 300;
+var y2;
+var x3;
+var y3= y1;
+var w=200;
+var h=200;
+
+
 
 var a,b,c;
+var aSlider;
+var bSlider;
 
 function setup(){
   createCanvas(400,400);
-  line(x1,y1,x2,y2);
-  line(x2,y2,x3,y3);
-  line(x1,y1,x3,y3);
+  smooth();
+  stroke(0);
+  aSlider=createSlider(y1-h,y1,[y1-h/2]); //y2
+  aSlider.position(300, 70);
+  bSlider=createSlider(x1,x1+w,[x1+w/2]); //x3
+  bSlider.position(300, 50);
 
 }
 
 function draw(){
-
-
+  background(255);
+  y2=map(aSlider.value(),y1-h,y1,y1,y1-h);
+  x3=(bSlider.value());
+  line(x1,y1,x2,y2);
+  line(x2,y2,x3,y3);
+  line(x1,y1,x3,y3);
 }
--- tutorials/trigonometry/unitCircle/sketch.js
+++ tutorials/trigonometry/unitCircle/sketch.js
@@ -11,6 +11,8 @@ var cy=200;
 
 var tx = 20;
 
+var ty = 50;
+
 var angleSlider;
 
 
@@ -36,15 +38,31 @@ function draw() {
   line(cx-d/2,cy,cx+d/2,cy);
   //big circle
   ellipse(cx,cy,d,d);
-  //line 1
+  fill(0);
+  //stroke(200);
   stroke(237,34,93);
   fill(237,34,93);
+  line(x1,y1,x1,cy); //opposite
+
+
+  line(x1,cy,cx,cy);
+
+
+  //line 1
+
   ellipse(cx,cy,5,5);
   ellipse(x1,y1,5,5);
   noFill();
 
   line(cx,cy,x1,y1);
+  console.log("angle="+angle);
+  if(angle==90||angle==0||angle==180||angle==270){
 
+  }else{
+  text("h",(x1+cx)/2+5,(cy+y1)/2+7);
+  text("x",(x1+cx)/2,cy-5);
+  text("y",x1-10,(y1+cy)/2);
+}
 
   //calculate x1
   x1=d/2*cos(radians(angle))+cx;
@@ -53,9 +71,7 @@ function draw() {
   stroke(0);
   //line 2
   ellipse(cx,cy,d,d);
-  fill(0);
-  stroke(200);
-  line(x1,y1,x1,cy);
+
 
   //curve
   noFill();
@@ -64,35 +80,36 @@ function draw() {
     //calculate y1
     y2=d/2*sin(radians(angle))+20;
     stroke(200);
-    arc(cx, cy, 60, 60, radians(angle),0);
-    // if(angle>180&&angle<270){
-    //   arc(cx, cy, 60, 60, PI*3, radians(angle));
-    //   stroke(237,34,93);
-    //   strokeWeight(3);
-    //   arc(cx, cy,300, 300,  PI*3, radians(angle));
-    //   strokeWeight(1);
-    // }
-    // if(angle>270&&angle<360){
-    //   arc(cx, cy, 60, 60, radians(angle), 0);
-    //   stroke(237,34,93);
-    //   strokeWeight(3);
-    //   arc(cx, cy,300, 300,  radians(angle), 0);
-    //   strokeWeight(1);
-    // }
-    // if(angle>0&&angle<90){
-    //   arc(cx, cy, 60, 60, 0,radians(angle));
-    //   stroke(237,34,93);
-    //   strokeWeight(3);
-    //   arc(cx, cy,300, 300,  0,radians(angle));
-    //   strokeWeight(1);
-    // }
-    // if(angle>90&&angle<180){
-    //   arc(cx, cy, 60, 60, radians(angle), PI);
-    //   stroke(237,34,93);
-    //   strokeWeight(3);
-    //   arc(cx, cy,300, 300,  radians(angle), PI);
-    //   strokeWeight(1);
-    // }
+
+   arc(cx, cy, 60, 60, radians(angle),0);
+  // if(angle>180&&angle<270){
+  //      arc(cx, cy, 60, 60, PI*3, radians(angle));
+  //      stroke(237,34,93);
+  //      strokeWeight(3);
+  //      //arc(cx, cy,300, 300,  PI*3, radians(angle));
+  //      strokeWeight(1);
+  //    }
+  //    if(angle>270&&angle<360){
+  //      arc(cx, cy, 60, 60, radians(angle), 0);
+  //      stroke(237,34,93);
+  //      strokeWeight(3);
+  //     // arc(cx, cy,300, 300,  radians(angle), 0);
+  //      strokeWeight(1);
+  //    }
+  //    if(angle>0&&angle<90){
+  //      arc(cx, cy, 60, 60, 0,radians(angle));
+  //      stroke(237,34,93);
+  //      strokeWeight(3);
+  //     // arc(cx, cy,300, 300,  0,radians(angle));
+  //      strokeWeight(1);
+  //    }
+  //    if(angle>90&&angle<180){
+  //      arc(cx, cy, 60, 60, radians(angle), PI);
+  //      stroke(237,34,93);
+  //      strokeWeight(3);
+  //     // arc(cx, cy,300, 300,  radians(angle), PI);
+  //      strokeWeight(1);
+  //    }
   stroke(0);
   //labels
   text("90",cx-5,20);
@@ -107,11 +124,24 @@ function draw() {
   fill(237,34,93);
   textSize(14);
   //key
-  text("Change the angle:",tx,50);
-  text("angle = "+round(360-angle) + " degrees",tx,height-120);
-  text("angle = "+(radians(360-angle)/PI).toFixed(2) + " \u03C0 radians",tx,height-150);
-  text("cos(angle) = "+cos(radians(angle)).toFixed(2),tx,height-90);
-  text("sin(angle) = "+sin(radians(angle)).toFixed(2),tx,height-60);
+  text("Change the angle:",tx,ty);
+  text("angle = "+round(360-angle) + " degrees",tx,ty+60);
+  text("angle = "+(radians(360-angle)/PI).toFixed(2) + " \u03C0 radians",tx,ty+80);
+
+  stroke(0);
+  fill(0);
+  textSize(12);
+  //text("SOH-CAH-TOA",tx,ty+160);
+
+  text("h = 1",tx,ty+160);
+
+  text("cos(angle) = x/h",tx,ty+200);
+  text("cos("+(radians(360-angle)/PI).toFixed(2) +" \u03C0) = x/1",tx,ty+220);
+  text(cos(radians(angle)).toFixed(2)+" = x",tx,ty+240);
+
+  text("sin(angle) = y/h",tx,ty+280);
+  text("sin("+(radians(360-angle)/PI).toFixed(2) +" \u03C0) = y/1",tx,ty+300);
+  text(-1*sin(radians(angle)).toFixed(2)+" = y",tx,ty+320);
 
 
 
